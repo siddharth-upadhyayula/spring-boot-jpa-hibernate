29d52c338ad8aadeb1e8ae364f9efb8497713a72
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2024-01-10 16:36:42 -0500

    Created the repository

---
+++ src/main/java/com/example/springbootjpahibernate/SpringBootJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaHibernateApplication.class, args);
+	}
+
+}
