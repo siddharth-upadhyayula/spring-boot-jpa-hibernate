74d345f37184c711c3f86a083c8e619f5c6e32a0
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2024-01-10 22:15:50 -0500

    Create SQL queries and talk to db using jdbc

---
+++ src/main/java/com/example/springbootjpahibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -0,0 +1,26 @@
+package com.example.springbootjpahibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+import com.example.springbootjpahibernate.course.Course;
+
+public class CourseJdbcCommandLineRunner implements CommandLineRunner{
+
+	@Autowired
+	private CourseJdbcRepository repository; 
+	
+	@Override
+	public void run(String... args) throws Exception {
+		repository.insert(new Course(1, "Learn AWS", "Sid"));	
+		repository.insert(new Course(1, "Learn AWS", "Sid"));
+		repository.insert(new Course(1, "Learn AWS", "Sid"));
+		
+	
+		
+		System.out.println(repository.findById(2));
+		System.out.println(repository.findById(2));
+		
+	}
+
+}
